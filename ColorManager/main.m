69d86181a7b9fabab1f04021bd4bff4453c8a8ea
颜风 <1178328901@qq.com>
2014-05-19 11:22:05 +0800

    Initial Commit

---
+++ ColorManager/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ColorManager
+//
+//  Created by   颜风 on 14-5-19.
+//  Copyright (c) 2014年 Shadow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
